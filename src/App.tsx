5edfb1d0fbca49e772209aa36b91cc3fbcaa46e8
Finn Bon <finn.bueno@gmail.com>
2024-12-28 16:32:32 +0100

    move handling of prefered color scheme to material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { AboutPage, HomePage, NotFoundPage } from "@src/pages";
 import { NavigationHeader } from "@src/templates";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+    light: true,
+  },
+});
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
       <BrowserRouter>
         <Routes>
           <Route element={<NavigationHeader />}>
@@ -14,7 +23,7 @@ const App = () => {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@src": "/src",
+      "@mui/styled-engine": "@mui/styled-engine-sc",
     },
   },
 });
